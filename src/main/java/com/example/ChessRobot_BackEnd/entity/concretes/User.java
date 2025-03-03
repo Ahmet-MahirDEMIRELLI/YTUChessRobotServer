ebe6d68c5b3b92ad037002e9b15a737e51e878d0
Ahmet Mahir Demirelli <122171884+Ahmet-MahirDEMIRELLI@users.noreply.github.com>
2025-03-03 21:55:02 +0300

    Update User.java

--- src/main/java/com/example/ChessRobot_BackEnd/entity/concretes/User.java
+++ src/main/java/com/example/ChessRobot_BackEnd/entity/concretes/User.java
@@ -6,7 +6,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
-
+import java.util.ArrayList;
 import java.util.Set;
 
 @Entity
