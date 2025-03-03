dce09e4e54d27bb6fa2f1ab310abe0c43efd2a48
Ahmet-MahirDEMIRELLI <a.mahir.demirelli@gmail.com>
2025-03-03 17:34:34 +0300

    isPinned and traceToKing functions added

--- src/main/java/com/example/ChessRobot_BackEnd/business/abstracts/ThreadCheckService.java
+++ src/main/java/com/example/ChessRobot_BackEnd/business/abstracts/ThreadCheckService.java
@@ -4,4 +4,5 @@ import com.example.ChessRobot_BackEnd.entity.dtos.Game.SquareDto;
 
 public interface ThreadCheckService {
     boolean isSquaresUnderThread(byte[][] board, SquareDto[] squaresToCheck, boolean isWhitePlaying);
+    int traceToKing(byte[][] board, SquareDto king, SquareDto pieceToCheck);
 }
--- src/main/java/com/example/ChessRobot_BackEnd/business/concretes/ThreadCheckManager.java
+++ src/main/java/com/example/ChessRobot_BackEnd/business/concretes/ThreadCheckManager.java
@@ -344,6 +344,121 @@ public class ThreadCheckManager implements ThreadCheckService {
         return false;
     }
 
+    /*
+    0 -> no trace from piece to king
+    1 -> king is on the right side of the piece
+    2 -> king is on the left side of the piece
+    3 -> king is on the lower side of the piece
+    4 -> king is on the upper side of the piece
+    5 -> king is on the up-left side of the piece
+    6 -> king is on the lower-left side of the piece
+    7 -> king is on the lower-right side of the piece
+    8 -> king is on the up-right side of the piece
+    */
+    @Override
+    public int traceToKing(byte[][] board, SquareDto king, SquareDto pieceToCheck) {
+        int rowDiff = king.getRow() - pieceToCheck.getRow();
+        int colDiff = king.getCol() - pieceToCheck.getCol();
+        int row = pieceToCheck.getRow();
+        int col = pieceToCheck.getCol();
+        if(rowDiff == 0 && colDiff != 0){  // same row
+            if(colDiff > 0){  // king is on the right side
+                col++;
+                while (col < king.getCol()){
+                    if(board[row][col] != 0){  // piece in between
+                        return 0;
+                    }
+                    col++;
+                }
+                return 1;
+            }
+            else{             // king is on the left side
+                col--;
+                while (col > king.getCol()){
+                    if(board[row][col] != 0){  // piece in between
+                        return 0;
+                    }
+                    col--;
+                }
+                return 2;
+            }
+        }
+        else if(colDiff == 0 && rowDiff != 0){  // same col
+            if(rowDiff > 0){  // king is on the lower side
+                row++;
+                while (row < king.getRow()){
+                    if(board[row][col] != 0){  // piece in between
+                        return 0;
+                    }
+                    row++;
+                }
+                return 3;
+            }
+            else{  // king is on the upper side
+                row--;
+                while (row > king.getRow()){
+                    if(board[row][col] != 0){  // piece in between
+                        return 0;
+                    }
+                    row--;
+                }
+                return 4;
+            }
+        }
+        else if(rowDiff == colDiff || rowDiff == -colDiff){  // same diagonal
+            if(rowDiff > 0 && colDiff > 0){       // king is on the up-left
+                row++;
+                col++;
+                while (row < king.getRow()){
+                    if(board[row][col] != 0){  // piece in between
+                        return 0;
+                    }
+                    row++;
+                    col++;
+                }
+                return 5;
+            }
+            else if(rowDiff < 0 && colDiff > 0){  // king is on the lower-left
+                row--;
+                col++;
+                while (row > king.getRow()){
+                    if(board[row][col] != 0){  // piece in between
+                        return 0;
+                    }
+                    row--;
+                    col++;
+                }
+                return 6;
+            }
+            else if(rowDiff < 0){    // king is on the lower-right
+                row--;
+                col--;
+                while (row > king.getRow()){
+                    if(board[row][col] != 0){  // piece in between
+                        return 0;
+                    }
+                    row--;
+                    col--;
+                }
+                return 7;
+            }
+            else if(rowDiff > 0){    // king is on the up-right
+                row++;
+                col--;
+                while (row < king.getRow()){
+                    if(board[row][col] != 0){  // piece in between
+                        return 0;
+                    }
+                    row++;
+                    col--;
+                }
+                return 8;
+            }
+        }
+
+        return 0;
+    }
+
     private boolean isSquareValid(int row, int col){
         return row >= 0 && row <= 7 && col >= 0 && col <= 7;
     }
